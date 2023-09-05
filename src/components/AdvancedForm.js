674298a90f3069401dc9962682c7776f6ea2a16e
Prakriti Pokharel <iprakritip@gmail.com>
2023-09-05 16:34:47 +0545

    use formik component to create form

--- src/components/AdvancedForm.js
+++ src/components/AdvancedForm.js
@@ -1,9 +1,31 @@
+import { Field, Form, Formik } from "formik";
+
 const AdvancedForm = () => {
   return (
-    <form autoComplete="off">
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" placeholder="Enter your username" />
-    </form>
+    <Formik
+      initialValues={{ name: '' }}
+    //  onSubmit={(values, actions) => {
+    //    setTimeout(() => {
+    //      alert(JSON.stringify(values, null, 2));
+    //      actions.setSubmitting(false);
+    //    }, 1000);
+    //  }}
+    >
+      {props => (
+        <Form>
+          <Field type="text" name="name" placeholder="Name" />
+          {/* <input
+            type="text"
+            onChange={props.handleChange}
+            onBlur={props.handleBlur}
+            value={props.values.name}
+            name="name"
+          /> */}
+          {/* {props.errors.name && <div id="feedback">{props.errors.name}</div>} */}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 export default AdvancedForm;
